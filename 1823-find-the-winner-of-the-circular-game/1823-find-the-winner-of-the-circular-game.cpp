f3825ce4007091264887dd1e1a70d07372e18638
satyamcsgupta <satyam.cs.gupta@gmail.com>
2026-08-01 19:22:47 +0530

    Time: 0 ms (100%), Space: 7.7 MB (95.2%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,34 +1,46 @@
 class Solution {
 public:
-int  winner(vector<bool>person , int n, int index ,int person_left, int k){
-    if(person_left== 1){
-        for(int i=0;i<n;i++){
-            if(person[i]==0){
-                return i;
-            }
-        }
+// int  winner(vector<bool>person , int n, int index ,int person_left, int k){
+//     if(person_left== 1){
+//         for(int i=0;i<n;i++){
+//             if(person[i]==0){
+//                 return i;
+//             }
+//         }
+//     }
+//   // finding the person to kill 
+//     int kill=(k-1)%person_left;
+//     while(kill--){
+//         index=(index+1)%n;
+//         while(person[index]==1){
+//               index=(index+1)%n;
+//         }
+
+//     }
+
+//     person[index]=1;
+//     while(person[index]==1){
+//         index=(index+1)%n;
+
+//     }
+
+//    return  winner(person,n,index,person_left-1,k);
+
+
+
+// }
+
+  int winner(int n,int k){
+    if(n==1){
+        return 0;
     }
-  // finding the person to kill 
-    int kill=(k-1)%person_left;
-    while(kill--){
-        index=(index+1)%n;
-        while(person[index]==1){
-              index=(index+1)%n;
-        }
 
-    }
-
-    person[index]=1;
-    while(person[index]==1){
-        index=(index+1)%n;
-
-    }
-
-   return  winner(person,n,index,person_left-1,k);
-
-}
+    return (winner(n-1,k)+k)%n;
+  }
     int findTheWinner(int n, int k) {
-        vector<bool>person(n,0);
-        return winner(person ,n,0,n,k)+1;
+        // vector<bool>person(n,0);
+        // return winner(person ,n,0,n,k)+1;
+
+        return winner(n , k)+1;
     }
 };
