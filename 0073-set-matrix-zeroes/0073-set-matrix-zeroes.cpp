35a9cbf620432d968d67fc93e2d028e0f2a429ee
satyamcsgupta <satyam.cs.gupta@gmail.com>
2026-08-05 21:48:29 +0530

    Time: 0 ms (100%), Space: 21.1 MB (21.45%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
-    void  making(vector<vector<int>>&nums,int i,int j,int r,int c){
-               for(int k=0;k<r;k++){
-                nums[k][j]=0;
-               }
-               for(int l=0;l<c;l++){
-                nums[i][l]=0;
-               }
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
 
-    }
-    void setZeroes(vector<vector<int>>& nums) {
-        int r=nums.size();
-        int c=nums[0].size();
-        vector<int> place;
-        int index=0;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(nums[i][j]==0){
-                   
-                    place.push_back(i);
-                    place.push_back(j);
+        vector<int> row(m, 0), col(n, 0);
 
+        // Mark rows and columns containing a zero
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
-      int n=place.size();
-      int k=0;
-       int index_j=0;
-       int index_i=0;
-        while(k<n){
-           index_i=place[k];
-           k++;
-           index_j=place[k];
-           
-            k++;
-            making(nums,index_i,index_j,r,c);
-           
-        } 
-     
+
+        // Set cells to zero
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
     }
 };
