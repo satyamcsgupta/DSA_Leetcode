a974394b2f1497016ef03fdf2cc4beb8f207b26c
satyamcsgupta <satyam.cs.gupta@gmail.com>
2026-08-05 20:12:50 +0530

    Time: 0 ms (100%), Space: 8.2 MB (74.59%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        vector<int> freq(26, 0);
-
-        for (char ch : word)
-            freq[ch - 'a']++;
-
-        sort(freq.begin(), freq.end(), greater<int>());
-
-        int ans = 0;
-
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] == 0) break;
-
-            int cost = i / 8 + 1;
-            ans += freq[i] * cost;
+        int n=word.size();
+        int res=0;
+        for(int i=0;i<n;i++){
+           res=res+(i/8+1);
         }
-
-        return ans;
+        return res;
     }
 };
